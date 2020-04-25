eb2db4364ad77871db1341bbac72b4a2e69ac91c
Alpha Man <xalphamanx@tuta.io>
2020-04-25 20:58:47 +0530

    Ticket buying page mostly done

--- resources/views/ticket/display.blade.php
+++ resources/views/ticket/display.blade.php
@@ -20,27 +20,44 @@
 
     <section class="has-margin-2">
         <div class="container">
-            {{--<div class="notification">
-                <div class="columns is-vcentered">
-                    <div class="column is-8">
-                        <p class="title is-4 has-text-grey-light">
-                            Selected: <span id="selected">0</span>
-                        </p>
-                    </div>
-                    <div class="column is-4 has-text-right">
-                        <p class="title is-4 has-text-grey-light">
-                            Total: <span id="total">0</span>
-                        </p>
-                    </div>
-                </div>
-            </div>--}}
             <div class="columns">
                 <div class="column is-8">
-                    <div class="card">
-                        <div class="card-content">
-
+                    @foreach($event->tickets as $ticket)
+                        <div class="card @if(! $loop->first) has-margin-2 @endif">
+                            <div class="card-content">
+                                <div class="columns level">
+                                    <div class="column is-6 level-item">
+                                        <p class="is-size-4 is-bold">{{ $ticket->name }}</p>
+                                        <p class="is-size-6">{{ $ticket->description }}</p>
+                                    </div>
+                                    <div class="column is-3 has-text-centered level-item">
+                                        <p class="is-size-5 is-bold">
+                                            @if($ticket->price == 0)
+                                                FREE
+                                            @else
+                                                {{ $ticket->price }} /-
+                                            @endif
+                                        </p>
+                                    </div>
+                                    <div class="column is-3 level-item">
+                                        <div class="columns">
+                                            <div class="column is-4">
+                                                <button class="button is-primary is-outlined is-fullwidth">-</button>
+                                            </div>
+                                            <div class="column is-4">
+                                                <div class="field">
+                                                    <input type="text" class="input has-text-centered" value="0" disabled>
+                                                </div>
+                                            </div>
+                                            <div class="column is-4">
+                                                <button class="button is-primary is-outlined is-fullwidth">+</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
                 <div class="column is-4">
                     <div class="card">
@@ -60,4 +77,10 @@
             </div>
         </div>
     </section>
+@endsection
+
+@section('scripts')
+    <script>
+
+    </script>
 @endsection
